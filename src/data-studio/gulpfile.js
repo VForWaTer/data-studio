91cd38215fe6937b4bd6d32a5c885a5934bfc383
Mirko Mälicke <mirko@hydrocode.de>
2022-11-19 16:58:21 +0100

    add a gulpfile to bundle a single file

---
+++ src/data-studio/gulpfile.js
@@ -0,0 +1,18 @@
+// found here: https://stackoverflow.com/questions/72440923/bundle-react-js-project-into-single-js-file-that-can-be-embedded-in-ghost-blog-p
+const gulp = require('gulp');
+const inlinesource = require('gulp-inline-source');
+const replace = require('gulp-replace');
+
+gulp.task('default', () => {
+  return gulp
+    .src('./build/*.html')
+    .pipe(replace('.js"></script>', '.js" inline></script>'))
+    .pipe(replace('rel="stylesheet">', 'rel="stylesheet" inline>'))
+    .pipe(
+      inlinesource({
+        compress: false,
+        ignore: ['png'],
+     })
+    )
+    .pipe(gulp.dest('./build'));
+});
