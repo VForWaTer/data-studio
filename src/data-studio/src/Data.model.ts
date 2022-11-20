7aced6d89f69fc23ea1b9b3f2c3f4557b02bb370
Mirko Mälicke <mirko@hydrocode.de>
2022-11-20 14:57:24 +0100

    define models

--- src/data-studio/src/Data.model.ts
+++ src/data-studio/src/Data.model.ts
@@ -3,14 +3,34 @@ export interface Data {
     datasets: Dataset[] 
 }
 
-export type DATASET_TYPE = 'ForceGraph' | 'Array'; 
+export type DATASET_TYPE = 'ForceGraph' | 'Array' | 'JSON' | 'Record'; 
 
 // basic Dataset
 export interface Dataset {
-    type: DATASET_TYPE
+    type: DATASET_TYPE,
     title: string,
 }
 
+// Record datasets
+export interface RecordDataset extends Dataset {
+    type: 'Record',
+    data: {[key: string]: any}[]
+}
+
+// Array dataset
+export interface ArrayDataset extends Dataset {
+    type: 'Array'
+    ndims?: number,
+    data: number[] | number[][]
+}
+
+// generic json-data
+export interface JSONDataset extends Dataset {
+    type: 'JSON',
+    data: any
+}
+
+// Force directed graph
 export interface GraphDataset extends Dataset {
     type: 'ForceGraph',
     data: GraphData
